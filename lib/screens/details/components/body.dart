3b6aac590fca31646bfc78cdfd553cfed0d9e85d
Huzaifa Athar <89946489+huzaifasharafat@users.noreply.github.com>
2023-06-14 01:17:16 +0500

    7

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:onlineshoui/constants.dart';
 import 'package:onlineshoui/models/Product.dart';
+import 'package:onlineshoui/screens/details/components/cart_counter.dart';
+import 'package:onlineshoui/screens/details/components/color_and_size.dart';
+import 'package:onlineshoui/screens/details/components/counter_with_fav_btn.dart';
+import 'package:onlineshoui/screens/details/components/description.dart';
 import 'package:onlineshoui/screens/details/components/product_title_with_image.dart';
 
 
@@ -43,6 +48,33 @@ class body extends StatelessWidget {
                   child: Column(
                     children: <Widget> [
                       ColorAndSize(product: product),
+                      Description(product: product),
+                      const CounterWithFavBtn(),
+                      Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: kDefaultPaddin
+                          ),
+                        child: Row(
+                          children: <Widget> [
+                            Container(
+                              height: 50,
+                              width: 58,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(18),
+                                border: Border.all(
+                                  color: product.color,
+                                )
+                              ),
+                              child: IconButton(
+                                icon: SvgPicture.asset(
+                                  "assets/icons/add_to_cart.svg"
+                                ),
+                                onPressed: () {} ,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -56,88 +88,6 @@ class body extends StatelessWidget {
   }
 }
 
-class ColorAndSize extends StatelessWidget {
-  const ColorAndSize({
-    super.key,
-    required this.product,
-  });
 
-  final Product product;
 
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: <Widget> [
-        const Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget> [
-              Text("Color"),
-              Row(children: <Widget> [
-                ColorDot(color: Color(0xff356c95), isSelected: true,),
-                ColorDot(color: Color(0xfff7c078)),
-                ColorDot(color: Color(0xffa29b9b)),
-              ],
-              ),
-            ],
-          ),
-        ),
-        Expanded(
-          child: RichText(
-              text: TextSpan(
-              style: const TextStyle(color: kTextColor),
-              children: [
-                const TextSpan( text: "Size\n"),
-                TextSpan(
-                  text: "${product.size} cm",
-                  style: Theme.of(context)
-                    .textTheme
-                    .headlineSmall
-                    ?.copyWith(fontWeight: FontWeight.bold)
-                )
-              ],
-          ),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class ColorDot extends StatelessWidget {
-  final Color color;
-  final bool isSelected;
-
-  const ColorDot({
-    super.key,
-    required this.color,
-    this.isSelected = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(
-        top: kDefaultPaddin/4,
-        right: kDefaultPaddin /2,
-
-      ),
-      padding: EdgeInsets.all(2.5),
-      height: 24,
-      width: 24,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        border: Border.all(
-          color: isSelected? color : Colors.transparent,
-        ),
-      ),
-      child: DecoratedBox(
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: color
-        ),
-      ),
-    );
-  }
-}
 
