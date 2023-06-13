0517a208970834f9f05b709e1dcc2e785d15bcb2
Huzaifa Athar <89946489+huzaifasharafat@users.noreply.github.com>
2023-06-13 21:19:59 +0500

    6

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:onlineshoui/constants.dart';
 import 'package:onlineshoui/models/Product.dart';
+import 'package:onlineshoui/screens/details/components/product_title_with_image.dart';
 
 
 class body extends StatelessWidget {
@@ -27,67 +28,25 @@ class body extends StatelessWidget {
                 Container(
 
                   margin: EdgeInsets.only(top: size.height *0.3),
-                  height: 500,
+                  padding: EdgeInsets.only(
+                      top: size.height*0.12,
+                      left: kDefaultPaddin,
+                      right: kDefaultPaddin,
+                  ),
+                  //height: 500,
                   decoration: const BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.vertical(top: Radius.circular(24))
+                    borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(24)
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: kDefaultPaddin),
                   child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-
                     children: <Widget> [
-                      const Text(
-                        "Aristocratic Hand Bag",
-                        style:  TextStyle(
-                          color: Colors.white
-                        ),
-                      ),
-                      Text(
-                          product.title,
-                        style:
-                        Theme.of(context)
-                        .textTheme
-                        .headlineSmall
-                        ?.copyWith(
-                            color : Colors.white,
-                            fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      SizedBox(height: kDefaultPaddin,),
-                      Row(
-                        children: <Widget>[
-                          RichText(
-                            text: TextSpan(
-                              children: [
-                                TextSpan(text: "Price"),
-                                TextSpan(
-                                  text: "\$${product.price}",
-                                  style: Theme.of(context)
-                                    .textTheme
-                                    .headlineSmall
-                                    ?.copyWith(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-
-                                  )
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(width: kDefaultPaddin,),
-                          Expanded(child: Image.asset(
-                              product.image,
-                              fit: BoxFit.fitWidth,
-                          )
-                          ),
-                        ],
-                      ),
+                      ColorAndSize(product: product),
                     ],
                   ),
-                )
+                ),
+                ProductTitleWithImage(product: product)
               ],
             ),
           )
@@ -96,3 +55,89 @@ class body extends StatelessWidget {
     );
   }
 }
+
+class ColorAndSize extends StatelessWidget {
+  const ColorAndSize({
+    super.key,
+    required this.product,
+  });
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget> [
+        const Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget> [
+              Text("Color"),
+              Row(children: <Widget> [
+                ColorDot(color: Color(0xff356c95), isSelected: true,),
+                ColorDot(color: Color(0xfff7c078)),
+                ColorDot(color: Color(0xffa29b9b)),
+              ],
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: RichText(
+              text: TextSpan(
+              style: const TextStyle(color: kTextColor),
+              children: [
+                const TextSpan( text: "Size\n"),
+                TextSpan(
+                  text: "${product.size} cm",
+                  style: Theme.of(context)
+                    .textTheme
+                    .headlineSmall
+                    ?.copyWith(fontWeight: FontWeight.bold)
+                )
+              ],
+          ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ColorDot extends StatelessWidget {
+  final Color color;
+  final bool isSelected;
+
+  const ColorDot({
+    super.key,
+    required this.color,
+    this.isSelected = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: kDefaultPaddin/4,
+        right: kDefaultPaddin /2,
+
+      ),
+      padding: EdgeInsets.all(2.5),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: isSelected? color : Colors.transparent,
+        ),
+      ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color
+        ),
+      ),
+    );
+  }
+}
+
